41ee5c960c0b6aa912721349f092bba23b2e8e39
Trinity999 <hanansmani@gmail.com>
2023-10-17 11:24:19 +0200

    more changes

--- src/renamer.py
+++ src/renamer.py
@@ -2,4 +2,11 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--dir")
+parser.add_argument("--dir", "-d", help="which dir")
+parser.add_argument("--file", "-f", help="which file", default="/tmp/filenames")
+args = parser.parse_args
+
+
+dir = args.dir
+filename = args.file
+
