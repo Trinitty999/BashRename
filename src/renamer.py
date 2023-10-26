122accee539813babfa079202437b7c383236cdb
Trinitty999 <hanansmani@gmail.com>
2023-10-26 22:18:54 +0200

    trying to add the bitch

--- src/renamer.py
+++ src/renamer.py
@@ -1,12 +1,21 @@
-import os
+from os import *
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--dir", "-d", help="which dir")
-parser.add_argument("--file", "-f", help="which file", default="/tmp/filenames")
-args = parser.parse_args
-
+parser.add_argument('dir', type=str, help="which dir")
+parser.add_argument("file", type=str, help="which files")
+args = parser.parse_args()
 
 dir = args.dir
-filename = args.file
 
+print(args.file)
+
+filedict = open(args.file, "r+")
+
+files = [f for f in listdir(dir) if path.isfile(path.join(dir, f))]    
+
+files.sort()
+
+for item in filename:
+    print(item)
+    
